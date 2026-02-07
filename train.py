fcd1d0c7fecb831be06dc6c05d6295175b805b8b
Anirudh <shivammm.hyphen@gmail.com>
2026-02-07 22:49:48 +0530

    add: Another Regression

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.ndimage import label
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, BaggingRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
